f11c184d270f28f6cb97af1a5eee8b0c1abe989e
Paul Holmes <paul@muckypuddle.com>
2017-01-24 10:30:18 +0000

    Sample project with MVE of heap issue. HeapProfiles contains before and after heap dumps.

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Forms9PatchHeapDemo.Droid
+{
+	[Activity(Label = "Forms9PatchHeapDemo.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			TabLayoutResource = Resource.Layout.Tabbar;
+			ToolbarResource = Resource.Layout.Toolbar;
+
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+
+			Forms9Patch.Droid.Settings.LicenseKey = "NZPK-RMP4-PJVV-Z7LP-78JF-GNXB-CDJZ-SRYA-BLR2-WBZC-G64K-QJZW-65DB";
+
+			LoadApplication(new App());
+		}
+	}
+}
---
+++ Forms9PatchHeapDemo/Forms9PatchHeapDemo.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Forms9PatchHeapDemo
+{
+	public class App : Application
+	{
+		public App()
+		{
+			// The root page of your application
+			var content = new ContentPage
+			{
+				Title = "Forms9Patch Demo",
+				Content = new StackLayout
+				{
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Forms9Patch.ImageButton
+						{
+							DefaultState = new Forms9Patch.ImageButtonState
+							{
+								BackgroundImage = new Forms9Patch.Image
+								{
+									Source = Forms9Patch.ImageSource.FromMultiResource("Forms9PatchHeapDemo.Resources.delay")
+								},
+							},
+							SelectedState = new Forms9Patch.ImageButtonState
+							{
+								BackgroundImage = new Forms9Patch.Image
+								{
+									Source = Forms9Patch.ImageSource.FromMultiResource("Forms9PatchHeapDemo.Resources.tick")
+								},
+							},
+							WidthRequest = 150,
+							HeightRequest = 150,
+							HorizontalOptions = LayoutOptions.Center, // needed for WidthRequest
+							ToggleBehavior = true,
+						}
+					}
+				}
+			};
+
+			MainPage = new NavigationPage(content);
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
